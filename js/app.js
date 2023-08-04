41c5d04694fcffdab653188d9b425592ce8ef2d9
dahee-lee <dahlee97@gmail.com>
2023-08-03 21:05:04 -0400

    initial files set up

---
+++ js/app.js
@@ -0,0 +1,16 @@
+class Player {
+    constructor(name){
+        this.name = name
+    }
+    //some methods (){} such as run, jump, stop for matcha+carrotcake
+}
+
+const fredRabbit = new Player("Fred")
+const bossRabbit = new Player("Monsieur Bossman")
+
+console.log(fredRabbit)
+console.log(bossRabbit)
+
+const runRabbitGameLogic = {
+
+}
