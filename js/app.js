526571e50e2f8fa48366816a3061620f5b55608e
dahee-lee <dahlee97@gmail.com>
2023-08-08 21:20:01 -0400

    Game logic 95% complete

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 //variables
+let mainPage = document.getElementById("mainPage")
 let rabbitRunGame = document.getElementById("rabbitRunGame")
 let fredsCommute = document.getElementById("fredsCommute")
 let bossCommute = document.getElementById("bossCommute")
@@ -6,98 +7,108 @@ let freddy = document.getElementById("freddy")
 let boss = document.getElementById("boss")
 let startButton = document.getElementById("startButton")
 let diceButton = document.getElementById("diceButton")
-//const winCondition = [
-    //fred position at 80%
-    //boss position at 80%
-//]
-
-let currentPlayer = freddy
-//check if game is currently running
-let running = false
+let endPage = document.getElementById("endPage")
+let restartBtn = document.getElementById("restart")
 
 class Player {
-    constructor(name){
+    constructor(name, character) {
         this.name = name,
-        this.distance = 0,
-        this.steps = null,
-        this.bankAccount = 0
-        }
-        //this.exObject = {}
-        //this.exArray = []
-        run(){
-            freddy.style.transform = "translate(20%, 20%)"
-        }
-
+            this.character = character, //the respective div elements
+            this.distance = 0,
+            this.steps = null,
+            this.bankAccount = 0
     }
-
-const fredRabbit = new Player("Fred")
-const bossRabbit = new Player("Monsieur Bossman")
-
-let players = [fredRabbit, bossRabbit]
-
-//console.dir(freddy)
-
-
-let winRound = false
-computerPlays=()=>{
-    if(currentPlayer === boss){
-        rollDie()
-        moveSteps()
-        checkWinner()
+    move(x) {
+        this.distance += x
+        console.log(`moving! new distance is ${this.distance}`)
+        this.character.style.left = this.distance + "%"
     }
 }
 
-/*
-class GameLogic {                                       
-        startGame(){
-            //startButton.addEventListener("click", ){
-                running = true
-            }
-            //diceButton.addEventListener("click", rollDie )
-        //add something to delay move until after number display
-        } 
-        rollDie(){
-            //add animation
-            //die generates a random number
-            let rollNum = Math.floor(Math.random()*3)
+const fredRabbit = new Player("Fred", freddy)
+const bossRabbit = new Player("Monsieur Bossman", boss)
 
-            //die displays number
-            moveSteps()
-            checkWinner()
-            changePlayer()
-        }
-        moveSteps(){
-            //player character moves that num of steps
-            currentPlayer.style.left += ""+rollNum+"%"
-        }
-        checkWinner(){
-            if(currentPlayer.style.left === "90%"){
-                rabbitRunGame.innerText = "you win!"
-                //make this a delayed animation w win msg
-            }else{
-                let winRound = false
-                changePlayer()
-            }
-        }
-        changePlayer(){
-            if(winRound === false && currentPlayer === freddy){
-                currentPlayer = boss
-                computerPlays()
-            }else if(winRound === false && currentPlayer === boss){
-                currentPlayer = freddy
-            }
-        //bold span to show which player's turn?
-        }
-        //computerPlays(){
-           
-//        }
-        reset(){
-            //eventlistener on reset button click 
-        }
+//Start with fred
+let currentPlayer = fredRabbit
+//game is not won yet
+let winRound = false
+//variable for random number for dice
+let diceRoll = 0
+
+startGame = () => {
+    console.log("starting game")
+    mainPage.hidden = "true"
+
+    diceButton.style.display = "block"
+    diceButton.addEventListener("click", () => {
+        diceRoll = rollDie()
+        fredRabbit.move(diceRoll)
+        checkWin()
+        changePlayer() //fred to boss
+        compRoll = compPlay()
+        bossRabbit.move(compRoll)
+        checkWin()
+        changePlayer() //end with fred again
+    })
+}
+
+compPlay = () => {
+    //roll die animation
+    //display diceRoll as text popup
+    console.log("computer is playing")
+    return Math.floor(Math.random() * 9 + 1)
+}
+
+changePlayer = () => {
+    if (winRound === false && currentPlayer === fredRabbit) {
+        currentPlayer = bossRabbit
+        console.log(currentPlayer) //"computer turn"
+        //add bossRabbit animation
+    } else if (winRound === false && currentPlayer === bossRabbit) {
+        currentPlayer = fredRabbit
+        diceButton.style.display = "block"
+        console.log(currentPlayer)
+        //add fredRabbit animation
     }
+}
 
+checkWin = () => {
+    if (currentPlayer.distance >= 90) {
+        console.log("game ends")
+        endPage.style.display = "block"
+        //add image and restart button
+        winRound = true
+        console.log("game is dun")
+        restart()
+    } else {
+        winRound = false
+        console.log("game is still goin")
+    }
+}
+
+rollDie = () => {
+    diceButton.style.display = "none"
+    console.log("rolling dice")
+    return Math.floor(Math.random() * 9 + 1)
+}
+
+/*
+resetGame = () => {
+    mainPage.hidden = "false"
+
+}
+
+restart = () => {
+    //eventlistener on reset button click 
+    restartBtn.addEventListener("click", resetGame)
+
+}
 */
-/* game logic
+
+startButton.addEventListener("click", startGame)
+
+/*   
+game logic
 starting page with animation and instructions
 player presses start
 start page disappears
@@ -109,6 +120,7 @@ player moves x steps
 game checks if anyone won
 game continues w comp turn
 computer turn
+display that it's comp turn
     computer presses roll die
     number generated
     boss moves x steps
@@ -124,6 +136,7 @@ game continues w player turn
 for game win check, if player/comp .style.left = 80%
 announce winner/loser
 end game
+----complete except animations ----
 restart button
 
 */
